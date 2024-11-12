651817443c6849bf119a75dcbe601ba591e278b0
F. Pisot <fps1001@alu.ubu.es>
2024-11-12 14:13:31 +0100

    ./project-app/project_app/lib y ./project-app/project_app/test

--- project-app/project_app/lib/sonar.bat
+++ project-app/project_app/lib/sonar.bat
@@ -2,4 +2,5 @@ sonar-scanner.bat `
   -D"sonar.organization=fps" `
   -D"sonar.projectKey=fps1001_ecocitytours" `
   -D"sonar.sources=." `
-  -D"sonar.host.url=https://sonarcloud.io"
+  -D"sonar.host.url=https://sonarcloud.io" `
+  -D"sonar.coverageReportPaths=coverage/lcov.info"
